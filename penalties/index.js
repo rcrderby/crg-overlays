1c32dc2dc84deaa9f2ad1a10d47fcee91d223a45
Timothy Hull <timothyhull@gmail.com>
2025-10-09 20:18:17 -0400

    Create function to initialize WebSocket listeners

--- penalties/index.js
+++ penalties/index.js
@@ -2,13 +2,18 @@
 $(function() {
   'use strict';
 
-  // Wait for WS to be loaded by core.js
+  // Wait for WebSocket listeners to be loaded by core.js
   function waitForWS() {
     if (typeof WS === 'undefined') {
       setTimeout(waitForWS, 100);
       return;
     }
     init();
+  }
+
+  // Initialize WebSocket listeners
+  function init() {
+    // TODO
   }
 
   // Start initialization when WS is ready
