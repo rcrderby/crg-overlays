69f0fccb23278fb0c7c50fe24d2aadc87b1d24e0
Timothy Hull <timothyhull@gmail.com>
2025-10-09 20:19:45 -0400

    Add WS listeners for each teams' data in the current game

--- penalties/index.js
+++ penalties/index.js
@@ -16,6 +16,10 @@ $(function() {
     // WebSocket connection using CRG ScoreBoard API
     WS.Connect();
     WS.AutoRegister();
+
+    // Listen for current game team data
+    WS.Register(['ScoreBoard.CurrentGame.Team(1)'], function(k, v) { updateTeam(1, k, v); });
+    WS.Register(['ScoreBoard.CurrentGame.Team(2)'], function(k, v) { updateTeam(2, k, v); });
   }
 
   // Start initialization when WS is ready
