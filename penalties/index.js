f20b220ab620aed6084b18934c6380de975ca933
Timothy Hull <timothyhull@gmail.com>
2025-10-09 20:15:50 -0400

    Add function to wait for WebSockets from CRG to prevent loading errors

--- penalties/index.js
+++ penalties/index.js
@@ -2,4 +2,15 @@
 $(function() {
   'use strict';
 
+  // Wait for WS to be loaded by core.js
+  function waitForWS() {
+    if (typeof WS === 'undefined') {
+      setTimeout(waitForWS, 100);
+      return;
+    }
+    init();
+  }
+
+  // Start initialization when WS is ready
+  waitForWS();
 });
