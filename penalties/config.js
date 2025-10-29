d9d5ecf79016751bafe554cc5753489060bde6d6
Timothy Hull <timothyhull@gmail.com>
2025-10-29 14:19:13 +0000

    Initial commit

---
+++ penalties/config.js
@@ -0,0 +1,10 @@
+/****************************************************************************
+** Configuration file for Roller Derby Penalties Overlay
+**
+** This file contains all customizable settings for the overlay.
+** 
+** This file must load within index.html before it loads core.js or index.js:
+** <script type="text/javascript" src="config.js"></script>
+** <script type="text/javascript" src="/json/core.js"></script>
+** <script type="text/javascript" src="index.js"></script>
+****************************************************************************/
