fb6cb19fc260910e06297342f3c84d50cf8e197f
Timothy Hull <timothyhull@gmail.com>
2025-10-29 14:29:02 +0000

    Add configuration variables sections

--- penalties/config.js
+++ penalties/config.js
@@ -8,3 +8,36 @@
 ** <script type="text/javascript" src="/json/core.js"></script>
 ** <script type="text/javascript" src="index.js"></script>
 ****************************************************************************/
+
+var PenaltiesOverlayConfig = {
+  /********************
+  ** Timing Settings **
+  ********************/
+
+  // TODO
+
+  /*********************
+  ** Display Settings **
+  *********************/
+
+  // TODO
+
+  /********************
+  ** Labels Settings **
+  ********************/
+
+  // TODO
+
+  /*******************
+  ** Rules Settings **
+  *******************/
+
+  // TODO
+
+  /***********************
+  ** Penalties Settings **
+  ***********************/
+
+  // TODO
+
+};
