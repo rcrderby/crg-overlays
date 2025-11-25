27d226ed3285d2933c5fb0e57990c93d06213952
Timothy Hull <timothyhull@gmail.com>
2025-11-25 17:49:17 +0000

    Uncomment constants for use

--- penalties/index.js
+++ penalties/index.js
@@ -50,8 +50,8 @@ console.log('Debug mode:', DEBUG);
 // Configuration sections - available globally for all functions
 const CONFIG = PenaltiesOverlayConfig.config;
 const LABELS = PenaltiesOverlayConfig.labels;
-// const RULES = PenaltiesOverlayConfig.rules;
-// const PENALTIES = PenaltiesOverlayConfig.penalties;
+const RULES = PenaltiesOverlayConfig.rules;
+const PENALTIES = PenaltiesOverlayConfig.penalties;
 const TIMING = PenaltiesOverlayConfig.timing;
 
 /**********************
